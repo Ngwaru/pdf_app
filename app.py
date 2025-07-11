14799a70a06473b25f3061945e84b9cdbaa553d3
Ngwaru Munodawafa <munodawafan@gmail.com>
2025-07-11 09:46:00 +0200

    Three New methods for the item_to_change on

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
 import streamlit as st
-from PyPDF2 import PdfFileMerger, PdfWriter, PdfReader
+from PyPDF2 import PdfMerger, PdfWriter, PdfReader
 from PIL import Image
 
 class Item_To_Change():
-    def __init__(self, pdf_files):
+    def __init__(self, pdf_files, pic_files=None):
         self.pdf_files = pdf_files
+        self.pic_files = pic_files
 
 
 
@@ -22,6 +23,23 @@ class Item_To_Change():
                     pdf_writer.add_page(Reader.pages[page_num])
                     with open(f"{output_path}\\page_num_{page_num}.pdf", "wb") as new_file:
                         pdf_writer.write(new_file)
+    def merge_pdf(self):
+        os.makedirs(".\\output", exist_ok=True)
+        output_path = ".\\output"
+        merger = PdfMerger(strict=False)
+        for file in self.pdf_files:
+            merger.append(file)
+        merger.write(f"{output_path}\\new_file.pdf")
+        merger.close()
+
+    def pdf_to_image(self):
+        os.makedirs(".\\output", exist_ok=True)
+        output_path = ".\\output"
+        for pic_file in self.pic_files:
+            pic_name = pic_file.split("\\")[-1].split('.')[-2]
+            image_content = Image.open(pic_file)
+            image_content_converted = image_content.convert("RGB")
+            image_content_converted.save(f"{output_path}\\{pic_name}.pdf")
 
 
 
@@ -36,6 +54,16 @@ def write_uploaded_folders(uploaded_files):
             current_file.write(f.getbuffer())
 
     return pdf_paths
+def get_pic_files(uploaded_files):
+    os.makedirs(".\\currrent_working_dir", exist_ok=True)
+    pic_paths = []
+    for f in uploaded_files:
+        if f.name.split(".")[-1] in ('png', 'jpg', 'jpeg'):
+            current_path = os.path.join(".\\currrent_working_dir", f.name)
+            pic_paths.append(current_path)
+            with open(current_path, "wb") as current_file:
+                current_file.write(f.getbuffer())
+    return pic_paths
 
 st.title("PDF Editing App")
 st.caption("Merge, Split pdf and change Images to PDF")
@@ -43,8 +71,8 @@ st.caption("Merge, Split pdf and change Images to PDF")
 
 
 
-uploaded_files = st.file_uploader("PDF Files", type='pdf', accept_multiple_files=True)
-if st.button("Upload Files"):
+uploaded_files = st.file_uploader("PDF Files", accept_multiple_files=True) 
+if st.button("Split PDF to pages"):
     if not uploaded_files:
         st.write("Upload Files")
     else:
@@ -52,17 +80,26 @@ if st.button("Upload Files"):
         
         item_to_work_on = Item_To_Change(pdfs)
         item_to_work_on.splitting()
-        if st.button("Split PDF to pages"):
-            print("start")
-            item_to_work_on.splitting()
             
+elif st.button("Merge PDF pages"):
+    if not uploaded_files:
+        st.write("Upload Files")
+    else:
+        pdfs = write_uploaded_folders(uploaded_files)
+        
+        item_to_work_on = Item_To_Change(pdfs)
+        item_to_work_on.merge_pdf()
+            
+elif st.button("Image to PDF"):
+    if not uploaded_files:
+        st.write("Upload Files")
+    else:
+        pic_files = get_pic_files(uploaded_files)
+        
+        item_to_work_on = Item_To_Change(pic_files, pic_files)
+        item_to_work_on.pdf_to_image()
+    
 
-        if st.button("Merge PDF pages"):
-            pass
 
-        if st.button("Image to PDF"):
-            pass
-
-
-        if st.button("PDF to Word"):
-            pass
+elif st.button("PDF to Word"):
+    pass
