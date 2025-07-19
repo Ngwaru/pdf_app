924192b7933b5a90e04a3e9e09eace9a1cca9345
Ngwaru Munodawafa <munodawafan@gmail.com>
2025-07-19 17:21:40 +0200

    Fixed the file system to work on linux

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import requests
 import streamlit as st
 from PyPDF2 import PdfMerger, PdfWriter, PdfReader
@@ -17,8 +18,8 @@ class Item_To_Change():
 
 
     def splitting(self):
-        os.makedirs(".\\output", exist_ok=True)
-        output_path = ".\\output"
+        os.makedirs("./output", exist_ok=True)
+        output_path = "./output"
         self.activated_Method = "splitting"
         for file in self.pdf_files:
             with open(file, "rb") as og_file:
@@ -26,40 +27,40 @@ class Item_To_Change():
                 for page_num in range(len(Reader.pages)):
                     pdf_writer = PdfWriter()
                     pdf_writer.add_page(Reader.pages[page_num])
-                    with open(f"{output_path}\\page_num_{page_num}.pdf", "wb") as new_file:
+                    with open(f"{output_path}/page_num_{page_num}.pdf", "wb") as new_file:
                         pdf_writer.write(new_file)
     def merge_pdf(self):
-        os.makedirs(".\\output", exist_ok=True)
-        output_path = ".\\output"
+        os.makedirs("./output", exist_ok=True)
+        output_path = "./output"
         self.activated_Method = "merge_pdf"
         merger = PdfMerger(strict=False)
         for file in self.pdf_files:
             merger.append(file)
-        merger.write(f"{output_path}\\new_file.pdf")
+        merger.write(f"{output_path}/new_file.pdf")
         merger.close()
 
     def pdf_to_image(self):
-        os.makedirs(".\\output", exist_ok=True)
-        output_path = ".\\output"
+        os.makedirs("./output", exist_ok=True)
+        output_path = "./output"
         self.activated_Method = "pdf_to_image"
         for pic_file in self.pic_files:
-            pic_name = pic_file.split("\\")[-1].split('.')[-2]
+            pic_name = pic_file.split("/")[-1].split('.')[-2]
             image_content = Image.open(pic_file)
             image_content_converted = image_content.convert("RGB")
-            image_content_converted.save(f"{output_path}\\{pic_name}.pdf")
+            image_content_converted.save(f"{output_path}/{pic_name}.pdf")
 
     def pdf_to_word(self):
-        os.makedirs(".\\output", exist_ok=True)
-        output_path = ".\\output"
+        os.makedirs("./output", exist_ok=True)
+        output_path = "./output"
         self.activated_Method = "pdf_to_word"        
         for file in self.pdf_files:
-            file_name = file.split("\\")[-1].split('.')[-2]
+            file_name = file.split("/")[-1].split('.')[-2]
             converter_one = Converter(file)
-            converter_one.convert(f"{output_path}\\{file_name}.docx")
+            converter_one.convert(f"{output_path}/{file_name}.docx")
 
     def rotate_pdf(self):
-        os.makedirs(".\\output", exist_ok=True)
-        output_path = ".\\output"
+        os.makedirs("./output", exist_ok=True)
+        output_path = "./output"
         self.activated_Method = "rotate_pdf" 
         if self.rotate == True:
             with open(self.pdf_files[0], "rb") as current_file:
@@ -71,28 +72,28 @@ class Item_To_Change():
                     if page_num in self.pages_to_rotate:
                         page.rotate(self.clockwise_rotation_degrees)
                     Writer.add_page(page)
-                with open(f"{output_path}\\rotated_new_file.pdf", "wb") as new_file:
+                with open(f"{output_path}/rotated_new_file.pdf", "wb") as new_file:
                     Writer.write(new_file)
                         
 
 
 
 def write_uploaded_folders(uploaded_files):
-    os.makedirs(".\\currrent_working_dir", exist_ok=True)
+    os.makedirs("./currrent_working_dir", exist_ok=True)
     pdf_paths = []
     for f in uploaded_files:
-        current_path = os.path.join(".\\currrent_working_dir", f.name)
+        current_path = os.path.join("./currrent_working_dir", f.name)
         pdf_paths.append(current_path)
         with open(current_path, "wb") as current_file:
             current_file.write(f.getbuffer())
 
     return pdf_paths
 def get_pic_files(uploaded_files):
-    os.makedirs(".\\currrent_working_dir", exist_ok=True)
+    os.makedirs("./currrent_working_dir", exist_ok=True)
     pic_paths = []
     for f in uploaded_files:
         if f.name.split(".")[-1] in ('png', 'jpg', 'jpeg'):
-            current_path = os.path.join(".\\currrent_working_dir", f.name)
+            current_path = os.path.join("./currrent_working_dir", f.name)
             pic_paths.append(current_path)
             with open(current_path, "wb") as current_file:
                 current_file.write(f.getbuffer())
@@ -160,22 +161,22 @@ elif st.button("Rotate PDF"):
             item_to_work_on = Item_To_Change(pdf_files=pdfs, rotate=True, pages_to_rotate=pages_to_rotate, clockwise_rotation_degrees=clockwise_rotation_degrees)
             item_to_work_on.rotate_pdf()
 
-if os.path.exists(".\\output"):
-    output_files = os.listdir(".\\output")
+if os.path.exists("./output"):
+    output_files = os.listdir("./output")
     st.markdown("<h2 style='text-align: center; color: blue; font-family: Arial, Helvetica, sans-serif;'> Download Documents below </h2>", unsafe_allow_html=True)
     for output_file in output_files:
-        output_file_path = os.path.join(".\\output", output_file)
+        output_file_path = os.path.join("./output", output_file)
         with open(output_file_path, "rb") as output_pdf_file:
             PDFbyte = output_pdf_file.read()
         st.download_button(label=output_file, data=PDFbyte, file_name=output_file, mime="application/octet-stream")
     if st.button("Clear"):
         for file in output_files:
-            remove_path = os.path.join(".\\output", file)
+            remove_path = os.path.join("./output", file)
             os.remove(remove_path)
-        if os.path.exists(".\\currrent_working_dir"):
-            input_files = os.listdir(".\\currrent_working_dir")
+        if os.path.exists("./currrent_working_dir"):
+            input_files = os.listdir("./currrent_working_dir")
         for input_file in input_files:
-            remove_path = os.path.join(".\\currrent_working_dir", input_file)
+            remove_path = os.path.join("./currrent_working_dir", input_file)
             os.remove(remove_path)
 
         st.cache_data.clear()
